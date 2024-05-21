7cb2c763e3cd6eba9ac8101ee796e9d71e45c92f
Sebastian Gabl <sebastian.gabl@gmx.at>
2024-05-21 22:09:48 +0200

    Set tase weapon data for all enemy weapons
    To prevent vanilla crashes with Tasers and the random weapon modifier

--- lua/charactertweakdata.lua
+++ lua/charactertweakdata.lua
@@ -4,5 +4,9 @@ Hooks:PostHook(CharacterTweakData, "init", "init_chaos_mod", function(self)
 	}
 	for _, enemy_name in pairs(self._enemy_list) do
 		setmetatable(self[enemy_name].weapon, weapon_mt)
+		for _, weapon_data in pairs(self[enemy_name].weapon) do
+			weapon_data.tase_sphere_cast_radius = self.taser.weapon.is_rifle.tase_sphere_cast_radius
+			weapon_data.tase_distance = self.taser.weapon.is_rifle.tase_distance
+		end
 	end
 end)
