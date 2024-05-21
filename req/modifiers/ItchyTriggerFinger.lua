52efd4cad21c010a2011ab495fb1ef205f1cf8da
Sebastian Gabl <sebastian.gabl@gmx.at>
2024-05-21 20:32:33 +0200

    Fix Itchy Trigger Finger modifier

--- req/modifiers/ItchyTriggerFinger.lua
+++ req/modifiers/ItchyTriggerFinger.lua
@@ -6,7 +6,7 @@ function ChaosModifierItchyTriggerFinger:start()
 	self._state = true
 
 	self:post_hook(PlayerStandard, "_get_input", function(playerstate, t)
-		if self._next_t and self._next_t < t then
+		if not self._next_t or self._next_t < t then
 			self._state = not self._state
 			self._next_t = t + (self._state and math.rand(0, 0.3) or math.rand(4, 8))
 		end
