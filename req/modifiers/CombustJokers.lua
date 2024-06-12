95d0fa43edc5383e2b1b55ddb78f05c4ae9234ca
Sebastian Gabl <sebastian.gabl@gmx.at>
2024-06-12 19:08:14 +0200

    Add "Spontaneous Combustion"

---
+++ req/modifiers/CombustJokers.lua
@@ -0,0 +1,34 @@
+ChaosModifierCombustJokers = ChaosModifier.class("ChaosModifierCombustJokers")
+
+function ChaosModifierCombustJokers:can_trigger()
+	return table.size(managers.groupai:state()._converted_police) > 0
+end
+
+function ChaosModifierCombustJokers:start()
+	for _, unit in pairs(managers.groupai:state()._converted_police) do
+		managers.explosion:play_sound_and_effects(unit:position(), math.UP, 300, {
+			effect = "effects/payday2/particles/explosions/grenade_incendiary_explosion",
+			sound_event = "white_explosion"
+		})
+	end
+
+	if Network:is_server() then
+		self:queue("combust", 0.5)
+	end
+end
+
+function ChaosModifierCombustJokers:combust()
+	for _, unit in pairs(managers.groupai:state()._converted_police) do
+		unit:character_damage():damage_fire({
+			variant = "fire",
+			damage = math.huge,
+			col_ray = {
+				unit = unit,
+				position = unit:position(),
+				ray = math.UP
+			}
+		})
+	end
+end
+
+return ChaosModifierCombustJokers
