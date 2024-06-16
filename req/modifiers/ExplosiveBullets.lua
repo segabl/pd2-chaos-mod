5b564b247490a4eb37355a660d31d9497854d0ad
Sebastian Gabl <sebastian.gabl@gmx.at>
2024-06-16 13:08:28 +0200

    Tweak modifiers
    Added a slight damage boost to "Heavy Munitions" to compensate for no headshots
    Made enemies chase players more directly during "Falcon Punch"

--- req/modifiers/ExplosiveBullets.lua
+++ req/modifiers/ExplosiveBullets.lua
@@ -5,6 +5,9 @@ function ChaosModifierExplosiveBullets:start()
 	self:post_hook(RaycastWeaponBase, "bullet_class", function()
 		return InstantExplosiveBulletBase
 	end)
+	self:post_hook(RaycastWeaponBase, "_get_current_damage", function(weaponbase)
+		return Hooks:GetReturn() * (weaponbase:bullet_class() == InstantExplosiveBulletBase and weaponbase._bullet_class == InstantBulletBase and 1.5 or 1)
+	end)
 end
 
 return ChaosModifierExplosiveBullets
--- req/modifiers/MeleeKnockback.lua
+++ req/modifiers/MeleeKnockback.lua
@@ -3,6 +3,9 @@ ChaosModifierMeleeKnockback.duration = 60
 
 function ChaosModifierMeleeKnockback:start()
 	self:override(CopBrain._logic_variants.security, "attack", TankCopLogicAttack)
+	self:override(CopLogicAttack, "_find_flank_pos", function(_, _, flank_tracker)
+		return flank_tracker:field_position()
+	end)
 	self:pre_hook(PlayerDamage, "damage_melee", function(_, data)
 		if data.push_vel then
 			mvector3.multiply(data.push_vel, 10)
