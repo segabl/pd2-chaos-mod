6eb889189a80262e6382ad19e39413ff4b8c4e52
Sebastian Gabl <sebastian.gabl@gmx.at>
2024-11-06 16:14:30 +0100

    Fix "Quantum Translocators" effect being host only

--- req/modifiers/TeleportingEnemies.lua
+++ req/modifiers/TeleportingEnemies.lua
@@ -1,6 +1,5 @@
 ChaosModifierTeleportingEnemies = ChaosModifier.class("ChaosModifierTeleportingEnemies")
 ChaosModifierTeleportingEnemies.run_in_stealth = false
-ChaosModifierTeleportingEnemies.run_as_client = false
 ChaosModifierTeleportingEnemies.duration = 30
 
 function ChaosModifierTeleportingEnemies:start()
@@ -14,27 +13,22 @@ function ChaosModifierTeleportingEnemies:start()
 		end
 
 		data.internal_data.warp_pos = data.objective.pos
-
-		if data.internal_data.warp_pos and (not data.teleport_start_effect_t or data.teleport_start_effect_t + 5 < data.t) then
-			data.teleport_start_effect_t = data.t
-			World:effect_manager():spawn({
-				effect = Idstring("effects/particles/explosions/explosion_flash_grenade"),
-				position = data.m_pos,
-				rotation = Rotation()
-			})
-		end
 	end)
 
-	self:pre_hook(CopLogicTravel, "_on_destination_reached", function(data)
-		if data.teleport_start_effect_t and (not data.teleport_stop_effect_t or data.teleport_stop_effect_t + 5 < data.t) then
-			data.teleport_stop_effect_t = data.t
-			World:effect_manager():spawn({
-				effect = Idstring("effects/particles/explosions/explosion_flash_grenade"),
-				position = data.m_pos,
-				rotation = Rotation()
-			})
+	local function spawn_effect(action, action_desc, common_data)
+		if not common_data.unit:in_slot(managers.slot:get_mask("enemies")) then
+			return
 		end
-	end)
+
+		World:effect_manager():spawn({
+			effect = Idstring("effects/particles/explosions/explosion_flash_grenade"),
+			position = common_data.unit:movement():m_pos(),
+			rotation = Rotation()
+		})
+	end
+
+	self:pre_hook(CopActionWarp, "init", spawn_effect)
+	self:post_hook(CopActionWarp, "init", spawn_effect)
 end
 
 return ChaosModifierTeleportingEnemies
