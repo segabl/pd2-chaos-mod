b1d988f365defcdfd195a527c3da94576f030798
Sebastian Gabl <sebastian.gabl@gmx.at>
2024-11-06 18:23:39 +0100

    Fix "Special Operation" not working as intended

--- req/modifiers/MoreSpecials.lua
+++ req/modifiers/MoreSpecials.lua
@@ -6,7 +6,9 @@ ChaosModifierMoreSpecials.color = "enemy_change"
 ChaosModifierMoreSpecials.duration = 90
 
 function ChaosModifierMoreSpecials:start()
-	self:override(tweak_data.group_ai, "special_unit_spawn_limits", table.collect(tweak_data.group_ai.special_unit_spawn_limits, function(l) return l * 3 end))
+	self:post_hook(JobManager, "current_spawn_limit", function()
+		return math.huge
+	end)
 
 	for _, group_ai_state_name in pairs({ "besiege", "street", "safehouse" }) do
 		for _, assault_state in pairs(tweak_data.group_ai[group_ai_state_name]) do
@@ -17,7 +19,9 @@ function ChaosModifierMoreSpecials:start()
 						for _, data in pairs(group.spawn) do
 							local special_type = tweak_data.group_ai.unit_categories[data.unit].special_type
 							if special_type and special_type ~= "medic" and data.amount_min and data.amount_min > 0 then
-								self:override(assault_state.groups, group_name, table.collect(weights, function(w) return w * 10 end))
+								for i, weight in pairs(weights) do
+									self:override(weights, i, weight * 10)
+								end
 								break
 							end
 						end
