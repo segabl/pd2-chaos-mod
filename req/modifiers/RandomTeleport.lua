c2ae11825c9e2ab2f381be7a4582a0f2c08eb404
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-02-02 20:01:57 +0100

    Drop bag before a teleport with "Teleporter Mishap"

--- req/modifiers/RandomTeleport.lua
+++ req/modifiers/RandomTeleport.lua
@@ -6,6 +6,7 @@ ChaosModifierRandomTeleport.interval = 3
 
 function ChaosModifierRandomTeleport:set_position(pos, force)
 	if pos and alive(self._player_unit) and (force or not self._player_unit:movement():on_zipline()) then
+		managers.player:drop_carry()
 		self._player_unit:warp_to(self._player_unit:rotation(), pos)
 	end
 end
