d92b80a4ba1b0d8879ff11515160b9bd93acf238
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-02-08 15:56:57 +0100

    Change trigger condition for "Modifier Overload"

--- req/modifiers/MaxModifiers.lua
+++ req/modifiers/MaxModifiers.lua
@@ -2,11 +2,11 @@ ChaosModifierMaxModifiers = ChaosModifier.class("ChaosModifierMaxModifiers")
 ChaosModifierMaxModifiers.run_as_client = false
 
 function ChaosModifierMaxModifiers:can_trigger()
-	return ChaosMod.settings.max_active > 1 and table.size(ChaosMod.active_modifiers) == 0
+	return table.size(ChaosMod.active_modifiers) <= math.floor(ChaosMod.settings.max_active / 3)
 end
 
 function ChaosModifierMaxModifiers:start()
-	for _ = 1, ChaosMod.settings.max_active do
+	for _ = table.size(ChaosMod.active_modifiers), ChaosMod.settings.max_active do
 		ChaosMod:activate_modifier()
 	end
 end
