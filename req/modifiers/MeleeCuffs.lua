3a0a1c50fdbf77d5417935610306183ddb184a1f
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-02-11 17:08:00 +0100

    Make enemies prefer getting close during "Restraining Order"

--- req/modifiers/MeleeCuffs.lua
+++ req/modifiers/MeleeCuffs.lua
@@ -3,6 +3,10 @@ ChaosModifierMeleeCuffs.loud_only = true
 ChaosModifierMeleeCuffs.duration = 60
 
 function ChaosModifierMeleeCuffs:start()
+	self:override(CopBrain._logic_variants.security, "attack", TankCopLogicAttack)
+	self:override(CopLogicAttack, "_find_flank_pos", function(_, _, flank_tracker)
+		return flank_tracker:field_position()
+	end)
 	self:post_hook(PlayerDamage, "damage_melee", function(playerdamage)
 		if Hooks:GetReturn() ~= "countered" and playerdamage._unit == managers.player:local_player() then
 			managers.player:set_player_state("arrested")
