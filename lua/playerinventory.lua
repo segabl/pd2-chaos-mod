a048ad4d323268ab3e2696fa8a14047a0924a231
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-04-07 22:13:43 +0200

    Fix PlayerInventory crash

--- lua/playerinventory.lua
+++ lua/playerinventory.lua
@@ -2,6 +2,7 @@ if StreamHeist then
 	return
 end
 
+local _place_selection_original = PlayerInventory._place_selection
 function PlayerInventory:_place_selection(selection_index, is_equip, ...)
 	local next_update_funcs_index = #_next_update_funcs
 
@@ -9,7 +10,7 @@ function PlayerInventory:_place_selection(selection_index, is_equip, ...)
 
 	if is_equip and #_next_update_funcs > next_update_funcs_index then
 		local update_func = _next_update_funcs[next_update_funcs_index + 1]
-		_next_update_funcs[#_next_update_funcs] = function()
+		_next_update_funcs[next_update_funcs_index + 1] = function()
 			if self._equipped_selection == selection_index then
 				update_func()
 			end
