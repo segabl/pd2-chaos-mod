1c37b1b1deb9228e90d73bf8a63b584761cccbbb
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-04-12 20:51:35 +0200

    Fix "Quantum Translocators" sometimes spamming teleports

--- req/modifiers/TeleportingEnemies.lua
+++ req/modifiers/TeleportingEnemies.lua
@@ -4,19 +4,23 @@ ChaosModifierTeleportingEnemies.duration = 45
 
 function ChaosModifierTeleportingEnemies:start()
 	self:pre_hook(CopLogicTravel, "upd_advance", function(data)
-		if data.internal_data.warp_pos or not data.unit:in_slot(managers.slot:get_mask("enemies")) then
+		if data.internal_data.warp_pos or not data.unit:movement():team().foes.criminal1 then
 			return
 		end
 
-		if not data.objective.pos and data.internal_data.coarse_path then
-			data.objective.pos = CopLogicTravel._get_exact_move_pos(data, #data.internal_data.coarse_path)
+		local warp_pos
+		if data.internal_data.coarse_path then
+			warp_pos = CopLogicTravel._get_exact_move_pos(data, #data.internal_data.coarse_path)
 		end
 
-		data.internal_data.warp_pos = data.objective.pos
+		if warp_pos and mvector3.distance_sq(warp_pos, data.m_pos) > 100 ^ 2 then
+			data.objective.pos = warp_pos
+			data.internal_data.warp_pos = warp_pos
+		end
 	end)
 
 	local function spawn_effect(action, action_desc, common_data)
-		if not common_data.unit:in_slot(managers.slot:get_mask("enemies")) then
+		if not common_data.unit:movement():team().foes.criminal1 then
 			return
 		end
 
