5c32fcbec8f17f19b8e9acaffd6547da7bdc4995
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-05-03 22:05:08 +0200

    Simplify some code

--- lua/hudmanager.lua
+++ lua/hudmanager.lua
@@ -1,6 +1,3 @@
 Hooks:PostHook(HUDManager, "update", "update_chaos_mod", function(self)
-	local cam = managers.viewport:get_current_camera()
-	if cam then
-		self._sound_source:set_position(cam:position())
-	end
+	self._sound_source:set_position(managers.viewport:get_current_camera_position())
 end)
