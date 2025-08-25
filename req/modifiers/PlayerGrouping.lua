596afe24e98456ea9964307ab89210c5f873d304
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-08-25 20:22:22 +0200

    Update visual effect of "Co-dependence"
    To make it more distinct from "Red Light, Green Light" and easier to notice when you are outside the safe distance

--- req/modifiers/PlayerGrouping.lua
+++ req/modifiers/PlayerGrouping.lua
@@ -1,54 +1,43 @@
 ChaosModifierPlayerGrouping = ChaosModifier.class("ChaosModifierPlayerGrouping")
 ChaosModifierPlayerGrouping.conflict_tags = { "PlayerDistance" }
 ChaosModifierPlayerGrouping.duration = 45
+ChaosModifierPlayerGrouping.radius = 600
 
 function ChaosModifierPlayerGrouping:can_trigger()
 	return table.size(managers.groupai:state():all_char_criminals()) > 1
 end
 
-function ChaosModifierPlayerGrouping:start()
-	self._lights = {}
-end
-
 function ChaosModifierPlayerGrouping:update(t, dt)
 	local criminals = managers.groupai:state():all_char_criminals()
 	local player_unit = managers.player:local_player()
-
-	for u_key, light in pairs(self._lights) do
-		if alive(light) then
-			if criminals[u_key] then
-				light:set_multiplier(25 + 10 * math.sin(t * 90))
-			else
-				World:delete_light(light)
-				self._lights[u_key] = nil
-			end
-		end
-	end
-
-	for u_key, data in pairs(criminals) do
-		if not self._lights[u_key] and alive(data.unit) and data.unit ~= player_unit then
-			local light = World:create_light("spot|specular")
-			light:set_spot_angle_end(179)
-			light:set_near_range(195)
-			light:set_far_range(625)
-			light:set_color(Vector3(0, 1, 0))
-			light:link(data.unit:orientation_object())
-			light:set_local_position(Vector3(0, 0, 200))
-			self._lights[u_key] = light
-		end
-	end
-
-	if not alive(player_unit) or self._next_t and self._next_t > t then
+	if not alive(player_unit) then
 		return
 	end
 
+	local player_pos = player_unit:movement():m_pos()
+	local brush = Draw:brush(Color.green)
 	for _, data in pairs(criminals) do
-		if alive(data.unit) and data.unit ~= player_unit then
-			local dis_sq = mvector3.distance_sq(data.unit:movement():m_pos(), player_unit:movement():m_pos())
-			if dis_sq < 600 ^ 2 then
-				return
-			end
+		if data.unit ~= player_unit and mvector3.distance_sq(data.m_pos, player_pos) < self.radius ^ 2 then
+			brush:circle(player_pos + math.UP, self.radius, 60, math.UP, 40)
+			return
 		end
+	end
+
+	local p = math.map_range(math.sin(t * 360 * 4), -1, 1, 0, 1)
+	brush:set_color(Color(1, 1 - p, 0))
+	brush:circle(player_pos + math.UP, self.radius, 60, math.UP, 40)
+
+	local pos_offset = Vector3()
+	for _, data in pairs(criminals) do
+		if data.unit ~= player_unit then
+			mvector3.step(pos_offset, player_pos, data.m_pos, self.radius - 30)
+			mvector3.set_z(pos_offset, player_pos.z + 1)
+			brush:line(pos_offset, data.m_pos + math.UP, 20)
+		end
+	end
+
+	if self._next_t and self._next_t > t then
+		return
 	end
 
 	player_unit:character_damage():damage_simple({
@@ -56,14 +45,6 @@ function ChaosModifierPlayerGrouping:update(t, dt)
 		damage = 0.5
 	})
 	self._next_t = t + 0.25
-end
-
-function ChaosModifierPlayerGrouping:stop()
-	for _, light in pairs(self._lights) do
-		if alive(light) then
-			World:delete_light(light)
-		end
-	end
 end
 
 return ChaosModifierPlayerGrouping
