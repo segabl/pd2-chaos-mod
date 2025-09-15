4c57cb9b7bcb13309d7a8c05e5bd9ede95d25b52
Sebastian Gabl <sebastian.gabl@gmx.at>
2025-09-16 00:02:26 +0200

    Tweak "Taste your own Medicine"
    Enemies will now be better at using grenade launchers

--- req/modifiers/CopyPlayerWeapons.lua
+++ req/modifiers/CopyPlayerWeapons.lua
@@ -35,6 +35,14 @@ function ChaosModifierCopyPlayerWeapons:get_weapon()
 end
 
 function ChaosModifierCopyPlayerWeapons:start()
+	local adjust_z_tbl = {
+		gre_m79 = true,
+		m32 = true,
+		china = true,
+		ms3gl = true,
+		slap = true
+	}
+
 	local function add_weapon(inventory, factory_id, blueprint, cosmetics)
 		HuskCopInventory.add_unit_by_factory_blueprint(inventory, factory_id, true, true, blueprint, cosmetics)
 
@@ -43,13 +51,38 @@ function ChaosModifierCopyPlayerWeapons:start()
 		weapon_base:set_ammo_max_per_clip(weapon_base:get_ammo_max_per_clip() + (stats.extra_ammo or 0))
 		weapon_base:set_ammo_remaining_in_clip(weapon_base:get_ammo_max_per_clip())
 
-		local _fire_raycast = weapon_base._fire_raycast
-		weapon_base._fire_raycast = function(weaponbase, user_unit, from_pos, direction, ...)
-			local weapon_tweak = tweak_data.weapon[weaponbase._name_id:gsub("_crew$", "")]
-			if not weapon_tweak or not weapon_tweak.projectile_type then
-				return _fire_raycast(weaponbase, user_unit, from_pos, direction, ...)
+		local name_id = weapon_base._name_id:gsub("_crew$", "")
+		local weapon_tweak = tweak_data.weapon[name_id]
+		if not weapon_tweak then
+			return
+		end
+
+		local projectile_type = weapon_tweak.projectile_type or weapon_tweak.projectile_types and next(weapon_tweak.projectile_types)
+		if weapon_base._ammo_data and weapon_base._ammo_data.launcher_grenade then
+			if weapon_tweak.projectile_types then
+				projectile_type = weapon_tweak.projectile_types[weapon_base._ammo_data.launcher_grenade] or weapon_base._ammo_data.launcher_grenade
+			else
+				projectile_type = weapon_base._ammo_data.launcher_grenade
 			end
-			ProjectileBase.throw_projectile_npc(weapon_tweak.projectile_type, from_pos, direction, user_unit)
+		end
+
+		local projectile_tweak = tweak_data.projectiles[projectile_type]
+		if not projectile_tweak then
+			return
+		end
+
+		local adjust_z = adjust_z_tbl[name_id] or adjust_z_tbl[weapon_tweak.based_on]
+
+		weapon_base._spawn_trail_effect = function() end
+		weapon_base._fire_raycast = function(_, user_unit, from_pos, direction, _, _, _, _, _, target_unit)
+			if alive(target_unit) then
+				direction = target_unit:position()
+				local dis = mvector3.distance(from_pos, direction)
+				local compensation = adjust_z and ((dis / 10) ^ 2) / ((projectile_tweak.launch_speed or 250) / 10) or 0
+				mvector3.set_z(direction, direction.z + compensation + 25)
+				mvector3.direction(direction, from_pos, direction)
+			end
+			ProjectileBase.throw_projectile_npc(projectile_type, from_pos, direction, user_unit)
 			return {}
 		end
 	end
